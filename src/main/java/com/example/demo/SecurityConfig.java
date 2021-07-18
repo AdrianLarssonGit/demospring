f07b66db78f302444dfc414459cb4ccc44eb3fbc
Adrian Larsson <developmentgitemail@protonmail.com>
2021-07-18 21:03:08 +0200

    Fixed SecurityConfig.java to work as intended.
    Before fix it extended wrong class as well as missed an import.

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -5,12 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.example.demo.UserDetailServiceImpl;
 
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfiguration {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
     @Autowired
--- src/main/java/com/example/demo/domain/User.java
+++ src/main/java/com/example/demo/domain/User.java
@@ -4,7 +4,6 @@ import javax.persistence.*;
 
 
 @Entity
-@Table(name="testingsome")
 public class User {
 
     @Id
